fccd0bee9eec74d746998aca1c69f2b7268db7a5
11249a207-ms <11249a207@kanchiuniv.ac.in>
2026-04-26 17:02:57 +0530

    First Fit.c

--- DAA/First Fit.c
+++ DAA/First Fit.c
@@ -1,46 +1,56 @@
 #include <stdio.h>
 
+// Function to implement First Fit algorithm
 void firstFit(int items[], int n, int capacity) 
 {
-    int bin[n];
-    int binCount = 0;
+    int bin[n];        // Array to store remaining capacity of bins
+    int binCount = 0;  // Number of bins used
 
     printf("\nExecuting First Fit Algorithm\n");
+
+    // Initialize all bins with 0 (no bins used yet)
     for (int i = 0; i < n; i++) 
     {
         bin[i] = 0;
     }
 
+    // Loop through each item
     for (int i = 0; i < n; i++) 
     {
+        // If item size is greater than bin capacity, skip it
         if (items[i] > capacity) 
         {
             printf("Item %d with size %d cannot be placed in any bin\n", i + 1, items[i]);
             continue;
         }
 
-        int placed = 0;
+        int placed = 0;  // Flag to check if item is placed
+
+        // Try to fit the item in existing bins
         for (int j = 0; j < binCount; j++)
         {
+            // If item fits in current bin
             if (bin[j] >= items[i]) 
             {
-                bin[j] -= items[i];
+                bin[j] -= items[i];  // Reduce remaining space
                 printf("Item %d placed in Bin %d\n", items[i], j + 1);
                 placed = 1;
                 break;
             }
         }
 
-        
+        // If item is not placed in any existing bin
         if (!placed) 
         {
+            // Create a new bin
             bin[binCount] = capacity;
-            bin[binCount] -= items[i];
+            bin[binCount] -= items[i];  // Place item in new bin
             printf("Item %d placed in Bin %d\n", items[i], binCount + 1);
-            binCount++;
+            binCount++;  // Increase bin count
         }
     }
 
+    // Print total bins used
     printf("Total bins used = %d\n", binCount);
 }
 
@@ -48,11 +58,13 @@ int main()
 {
     int n, capacity;
 
+    // Input number of items
     printf("Enter number of items: ");
     scanf("%d", &n);
 
-    int items[n];
+    int items[n];  // Array to store item sizes
 
+    // Input item sizes
     printf("Enter item sizes:\n");
     for (int i = 0; i < n; i++) 
     {
@@ -60,10 +72,12 @@ int main()
         scanf("%d", &items[i]);
     }
 
+    // Input bin capacity
     printf("Enter bin capacity: ");
     scanf("%d", &capacity);
 
+    // Call First Fit function
     firstFit(items, n, capacity);
 
-    return 0;
+    return 0;  // End of program
 }
