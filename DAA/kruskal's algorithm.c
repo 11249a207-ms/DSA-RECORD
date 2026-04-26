2c7533f79da8fd4c2bc908802ff6ce324c2c81ad
11249a207-ms <11249a207@kanchiuniv.ac.in>
2026-04-26 17:16:23 +0530

    kruskal's algorithm.c

--- DAA/kruskal's algorithm.c
+++ DAA/kruskal's algorithm.c
@@ -1,20 +1,28 @@
 #include <stdio.h> 
 #define V 5 
 #define E 6 
+
+// This array keeps track of who reports to whom.
+// It’s basically the backbone of the Disjoint Set Union (DSU) data structure.
 int parent[V]; 
  
+// The 'Find' operation: it climbs up the family tree until it finds the "boss" (Leader)
 int findLeader(int v) 
 { 
     while (parent[v] != v) 
         v = parent[v]; 
     return v; 
 } 
+
+// The 'Union' operation: makes one group report to the leader of the other group
 void mergeSets(int u, int v) 
 { 
     int a = findLeader(u); 
     int b = findLeader(v); 
     parent[a] = b; 
 }
+
+// Kruskal's is "Greedy," so we HAVE to sort edges by weight first
 void sortEdges(int edges[E][3]) 
 { 
     for (int i = 0; i < E - 1; i++) 
@@ -23,6 +31,7 @@ void sortEdges(int edges[E][3])
         { 
             if (edges[j][2] > edges[j + 1][2]) 
              { 
+                // Swapping the whole edge (u, v, and weight)
                 for (int k = 0; k < 3; k++) 
                 { 
                     int temp = edges[j][k]; 
@@ -37,6 +46,8 @@ void sortEdges(int edges[E][3])
 void kruskal(int edges[E][3]) 
 { 
     sortEdges(edges); 
+    
+    // Every node starts as its own boss
     for (int i = 0; i < V; i++) 
         parent[i] = i; 
  
@@ -49,13 +60,17 @@ void kruskal(int edges[E][3])
         int v = edges[i][1]; 
         int w = edges[i][2]; 
 
+        /* CYCLE CHECK: If the leaders of both nodes are different, 
+           it means they aren't connected yet. Adding this edge won't create a loop. */
         if (findLeader(u) != findLeader(v)) 
         { 
             printf("%d -- %d (Weight: %d)\n", u, v, w); 
-            mergeSets(u, v); 
+            mergeSets(u, v); // Connect them!
             cost += w; 
             count++; 
         }
+
+        // Optimization: An MST always has exactly V-1 edges. If we hit that, we're done.
         if (count == V - 1) 
             break; 
     } 
@@ -64,6 +79,7 @@ void kruskal(int edges[E][3])
  
 int main() 
 { 
+    // Edge list format: {Source, Destination, Weight}
     int edges[E][3] = { 
         {0, 1, 2}, {0, 2, 3}, {1, 2, 5}, {1, 3, 3}, {2, 4, 4}, {3, 4, 2}}; 
  
